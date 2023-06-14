1ab212f85e03bbeecadd50d82a41250c34df7f9d
JordanPalafox <a00835705@tec.mx>
2023-06-14 01:45:32 -0600

    modified:   src/movie.h 	modified:   src/main.exe

--- src/movie.h
+++ src/movie.h
@@ -3,7 +3,7 @@
 class Movie : public Video
 {
 public:
-    Movie(int id, std::string name, double duration, std::string genre, double rating);
+    Movie(int id, string name, double duration, string genre, double rating);
 
     void calculateAverageDuration() override;
     void calculateAverageRating() override;
